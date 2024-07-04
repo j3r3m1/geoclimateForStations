26d1e6e8ce95b1fb20d07e1998947e39857ac009
BERNARD Jeremy <bernardj@pxdesrtls024>
2024-07-04 15:09:47 +0200

    Now works for OSM

--- LczForStationBuffer/src/main/groovy/Main.groovy
+++ LczForStationBuffer/src/main/groovy/Main.groovy
@@ -3,41 +3,42 @@
 @Grab(group='org.orbisgis.geoclimate', module='geoclimate', version='1.0.1-SNAPSHOT')
 
 // Remove comment in IntelliJ
-package org.orbisgis.geoclimate.geoindicators
+// package org.orbisgis.geoclimate.geoindicators
 
 import org.orbisgis.data.jdbc.JdbcDataSource
 import static org.orbisgis.data.H2GIS.open
 import org.orbisgis.geoclimate.Geoindicators
 
 // Path of the file containing the buffer geometries around stations
-File buffer_dir = new File('/home/decide/Software/GeoClimate/osm_Pont-de-Veyle/rsu.fgb')
+File buffer_dir = new File(args[0])
 
-// Path and name of the resulting file with LCZ type for each station buffer
-String outputPath = '/tmp/LCZ_FOR_STATION_BUFFER.fgb'
+// Path where the resulting files with LCZ type and indicators for each station buffer will be saved
+String outputPath = args[1]
 
 // Directory where are saved the GeoClimate layers
-String geoclimate_dir = '/home/decide/Software/GeoClimate/osm_Pont-de-Veyle'
+String geoclimate_dir = args[2]
 
 // Name of the file format extension (for GeoClimate output files)
 String extension = ".fgb"
 
 // Name of the ID station column (the id should be an integer)
-String ID_station = "id_rsu"
+String ID_station = args[3]
 
-// Name of the file and table containing all unit with corresponding LCZ
-String outputTab = "LCZ_FIN"
 
 // Modify initial input parameters (set only LCZ calculation)
 Map input_params = Geoindicators.WorkflowGeoIndicators.getParameters()
-input_params["indicatorUse"] = ["LCZ"]
+input_params["indicatorUse"] = ["LCZ", "TEB", "UTRF"]
 
 // Open an H2GIS connection
 h2GIS = open(File.createTempDir().toString() + File.separator + "myH2GIS_DB;AUTO_SERVER=TRUE")
 
-main(geoclimate_dir, h2GIS, input_params, outputTab, buffer_dir, outputPath, ID_station, extension)
+main(geoclimate_dir, h2GIS, input_params, buffer_dir, outputPath, ID_station, extension)
 
-static void main(String inputDir, JdbcDataSource h2GIS, Map input_params, String outputTab, File buffer_dir,
+static void main(String inputDir, JdbcDataSource h2GIS, Map input_params, File buffer_dir,
                  String outputPath, String ID_station, String extension) {
+  String outputLcz = "RSU_LCZ"
+  String outputIndic = "RSU_INDICATORS"
+  
   String extension_tmp
   // Load GeoClimate files into the Database
   for (l in ["zone", "rail", "road", "building", "vegetation",
@@ -50,11 +51,11 @@ static void main(String inputDir, JdbcDataSource h2GIS, Map input_params, String
       extension_tmp = extension
     }
     File filename = new File(inputDir + File.separator + l + extension_tmp)
-    if (filename.exists()){
+    if (filename.exists() and filename.length() != 0){
       h2GIS.load(filename.toString(), l)
     }
     else{
-      println(filename.toString() + " do not exist")
+      println(filename.toString() + " do not exist or is empty")
     }
   }
   // Load the RSU (which are the buffer around station) into the database
@@ -67,8 +68,9 @@ static void main(String inputDir, JdbcDataSource h2GIS, Map input_params, String
   }
 
   // Create a table where to append all results at the end
-  List queryFin = []
-  Map rsu_lcz_list = [:]
+  List queryFinLcz = []
+  List queryFinIndic = []
+  Map rsu_list = [:]
 
   // Run the calculation for each unit (buffer circle around station)
   int i = 0
@@ -83,7 +85,7 @@ static void main(String inputDir, JdbcDataSource h2GIS, Map input_params, String
     // Need to delete the potential existing id_rsu in the building table
     h2GIS """DROP TABLE IF EXISTS building_tempo;
             CREATE TABLE building_tempo
-                AS SELECT * EXCEPT(ID_RSU)
+                AS SELECT * EXCEPT(ID_RSU, ID_BLOCK)
                 FROM building"""
 
     // Create a table containing a single unit...
@@ -91,9 +93,15 @@ static void main(String inputDir, JdbcDataSource h2GIS, Map input_params, String
                     CREATE TABLE RSU$id_rsu
                       AS SELECT * FROM RSU
                       WHERE ID_RSU = $id_rsu"""
-    rsu_lcz_list[id_rsu] = Geoindicators.WorkflowGeoIndicators.computeAllGeoIndicators(
+                     
+    // Restrict the zone to the buffer
+    h2GIS """DROP TABLE IF EXISTS zone_tempo;
+            CREATE TABLE zone_tempo
+                AS SELECT the_geom as the_geom, 'rsu$id_rsu' as ID_RSU
+                FROM rsu$id_rsu"""
+    rsu_list[id_rsu] = Geoindicators.WorkflowGeoIndicators.computeAllGeoIndicators(
                                h2GIS,
-                              "zone",
+                              "zone_tempo",
                               "building_tempo",
                               "road",
                               "rail",
@@ -105,18 +113,24 @@ static void main(String inputDir, JdbcDataSource h2GIS, Map input_params, String
                               "urban_areas",
                               "RSU$id_rsu",
                               input_params,
-                              "rsu$id_rsu")["rsu_lcz"]
+                              "rsu$id_rsu")
 
     // Add the table to the list of tables to union
-    queryFin.add("SELECT * FROM ${rsu_lcz_list[id_rsu]}")
+    queryFinLcz.add("SELECT * FROM ${rsu_list[id_rsu]['rsu_lcz']}")
+    queryFinIndic.add("SELECT * FROM ${rsu_list[id_rsu]['rsu_indicators']}")
 
   }
-  // Union all LCZ
-  h2GIS """DROP TABLE IF EXISTS $outputTab;
-                      CREATE TABLE $outputTab
-                      AS ${queryFin.join(" UNION ALL ")};
-                ALTER TABLE $outputTab RENAME COLUMN id_rsu TO $ID_station"""
+  // Union all buffers for LCZ and indicators
+  h2GIS """DROP TABLE IF EXISTS $outputLcz;
+                      CREATE TABLE $outputLcz
+                      AS ${queryFinLcz.join(" UNION ALL ")};
+                ALTER TABLE $outputLcz RENAME COLUMN id_rsu TO $ID_station"""
+  h2GIS """DROP TABLE IF EXISTS $outputIndic;
+                    CREATE TABLE $outputIndic
+                    AS ${queryFinIndic.join(" UNION ALL ")};
+            ALTER TABLE $outputIndic RENAME COLUMN id_rsu TO $ID_station"""
 
   // Save results
-  h2GIS.save(outputTab, outputPath)
+  h2GIS.save(outputLcz, outputPath + File.separator + "${outputLcz}.fgb")
+  h2GIS.save(outputIndic, outputPath + File.separator + "${outputIndic}.fgb")
 }
